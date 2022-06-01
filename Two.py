51bec1698eba2beb03d84c5d16de702be911c776
Rahul6009 <106512278+Rahul6009@users.noreply.github.com>
2022-06-01 21:35:55 +0530

    Add files via upload

---
+++ Two.py
@@ -0,0 +1,26 @@
+#basic Arithmetic operation
+'''
+a=10
+print("a  =  ",a)
+print("type  :  ",type(a))
+a=10.1132
+print("a  =  ",a)
+print("type  :  ",type(a))
+a="jemes"
+print("a  =  ",a)
+print("type  :  ",type(a))
+a=True
+print("a  =  ",a)
+print("type  :  ",type(a))
+'''
+a,b=10,20
+
+a=int(input("Enter  a:"))
+b=int(input("Enter b: "))
+
+c=a+b
+print("sum of  ",a," and ",b," is ",c)
+c=a-b
+print("sub of  ",a," and ",b," is ",c)
+c=a*b
+print("mul of  ",a," and ",b," is ",c)
---
+++ one.py
@@ -0,0 +1,7 @@
+#This is my first python Application
+
+print("Welcome To Python.")
+
+print("Hello World")
+
+print("This is my first Application")
